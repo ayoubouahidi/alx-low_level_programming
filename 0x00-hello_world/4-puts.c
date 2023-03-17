daffc48ce355d3d64fb32163ee45da8e3f28d803
ayoubouahidi <ouahidi.ayoub2002@gmail.com>
2023-03-16 17:24:44 -0700

    stylo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- * main - programme that print "Programming is like building a multilingual puzzle"
+ * main - programme that print Programming is like building a multilingual puzzle
  *
- * return: Always 0 (success)
+ * Return: Always 0 (success)
  */
 int main (void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-		return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return 0;
 }
