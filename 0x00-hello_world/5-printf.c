6d44b346655155cb252853b9f9e32685103e5f2b
ayoubouahidi <ouahidi.ayoub2002@gmail.com>
2023-03-16 18:25:04 -0700

    rpp

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
-#include<stdio.h> 
+#include<stdio.h>
+
 /**
- * main -AA proframme that print with a printf
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
