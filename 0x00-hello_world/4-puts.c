9f1f537d91ff0ede454b52270acca5749bb30117
ayoubouahidi <ouahidi.ayoub2002@gmail.com>
2023-03-16 17:21:30 -0700

    dream on

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - programme that print "Programming is like building a multilingual puzzle"
+ *
+ * return: Always 0 (success)
+ */
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+		return 0;
+}
