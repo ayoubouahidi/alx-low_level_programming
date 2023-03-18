af3b594c0da20fcb87d3b0d2da7b3ca2f6794ffb
ayoubouahidi <ouahidi.ayoub2002@gmail.com>
2023-03-17 19:09:05 -0700

    dream on

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * description: prints the alphabet in lowercase, followed by a new line
+ * Return : Always 0
+ */
+int main(void)
+{
+	putchar(65);
+	putchar('\n');
+	return (0);
+}
