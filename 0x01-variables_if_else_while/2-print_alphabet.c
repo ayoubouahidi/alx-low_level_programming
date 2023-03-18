861f32ea0a9609273f43adbf7be8763084ec6741
ayoubouahidi <ouahidi.ayoub2002@gmail.com>
2023-03-18 10:53:28 -0700

    dream

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /**
  * main - Entry point
- * description: prints the alphabet in lowercase, followed by a new line. 
+ * description: prints the alphabet in lowercase followed by a new line.
  * Return : Always 0
  */
 
