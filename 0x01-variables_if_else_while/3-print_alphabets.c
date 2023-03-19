8a6653cff21d6116d4794b004c795106e6db2048
ayoubouahidi <ouahidi.ayoub2002@gmail.com>
2023-03-19 10:20:08 -0700

    bread new

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * description: print alphabets in lowercase and uppercase
+ * Return: Always 0
+ */
+
+int main(void)
+
+{
+		int n = 97;
+		int m = 65;
+
+		while (n <= 122)
+		{
+			putchar(n);
+			n++;
+		}
+
+		while (m <= 90)
+		{
+			putchar(m);
+			m++;
+		}
+
+		putchar('\n');
+		return (0);
+}
+
