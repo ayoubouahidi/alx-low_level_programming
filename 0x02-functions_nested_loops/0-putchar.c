f4d5e199122aa9ba08ab6af88110c09ebdd8bb9e
ayoubouahidi <ouahidi.ayoub2002@gmail.com>
2023-03-21 16:27:32 -0700

    dream on

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main -Entry point
+ * description: programme prints putchar
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Hello evryonr here is project in fonction in c so enjoy watching this anw i m proud of being here and i do what i like 
