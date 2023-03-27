bbf682a5360b2800864a5e307fd2fde552ef0866
ayoubouahidi <ouahidi.ayoub2002@gmail.com>
2023-03-27 16:04:29 -0700

    dream on

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,19 @@
-#include "main.h"
-/**
- * main - check code
- * description: int as paramater
- * Return : Always 0
- */
+#include <stdio.h>
 
-void reset_to_98(int *n);
+void reset_to_98(int *);
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-	*n = 98;
+	int n;
+
+	n = 402;
+	printf("%d\n", n);
+	reset_to_98(&n);
+	printf("%d\n", n);
+	return (0);
 }
