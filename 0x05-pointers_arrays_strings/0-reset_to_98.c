59bcfc1f343c7d0ab71ff70c522117095e341570
ayoubouahidi <ouahidi.ayoub2002@gmail.com>
2023-03-27 15:55:03 -0700

    dream on

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main"
+#include<stdio.h>
+
+/**
+ * main - check code
+ * description: int as paramater
+ * Return : Always 0
+ */
+
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+HERE IS MY PROJECT FOR POINTERS. and arraya .
+all u need for project here and i m happy to see u here.
