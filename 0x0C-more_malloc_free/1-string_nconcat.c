d95273747e5fb4d8ceefb9f0938dc4d0791e97d3
ayoubouahidi <ouahidi.ayoub2002@gmail.com>
2023-04-14 02:47:58 -0700

    dream on

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -5,7 +5,7 @@
  * @s1 : first string
  * @s2 : second string
  * @n : index
- * Return : If the function fails, it should return NULL
+ * Return: If the function fails, it should return NULL
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
